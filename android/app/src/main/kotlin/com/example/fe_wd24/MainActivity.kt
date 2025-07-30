7460825867f928410c8a2bd7a16713ac5f8e0425
minhduc <nguyenduc123az321@gmail.com>
2025-07-30 21:29:41 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fe_wd24/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fe_wd24
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:fe_wd24/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:tailwind_colors/tailwind_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1200, name: DESKTOP),
+        ],
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: TWColors.gray.shade50,
+        textTheme: GoogleFonts.playfairDisplayTextTheme(),
+        colorScheme: ColorScheme.fromSeed(seedColor: TWColors.pink.shade300),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
