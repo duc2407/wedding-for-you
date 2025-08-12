fc2abe61d863c15b900b498668f6487f9261d137
duc2407 <duc2407@users.noreply.github.com>
2025-08-12 19:14:06 +0000

    deploy: f7f10ae79f9d477733e7b4d37deae84c1e3681a9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1dee29e67723b8393ddb47bd62af4c88",
+const RESOURCES = {"flutter_bootstrap.js": "2a1beb06dc8c912ac270940f2669006f",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
